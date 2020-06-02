05f96d4f2e25a804c77be7b55e054ec5a7167ac7
MisaelVillaverde <misaelvilla10@gmail.com>
2020-06-02 16:41:43 -0500

    Updates

---
+++ precache-manifest.638d777c7eb3137e29867a26deadf992.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a97840036f481d2c78fc55a5e5e4bf24",
+    "url": "/deploy-react-app/index.html"
+  },
+  {
+    "revision": "1b07d883da8c4915571c",
+    "url": "/deploy-react-app/static/css/main.6b19969c.chunk.css"
+  },
+  {
+    "revision": "17d745d487a2e96ee3b2",
+    "url": "/deploy-react-app/static/js/2.8680e18e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react-app/static/js/2.8680e18e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b07d883da8c4915571c",
+    "url": "/deploy-react-app/static/js/main.54926f61.chunk.js"
+  },
+  {
+    "revision": "4e3e7b952f31511d207a",
+    "url": "/deploy-react-app/static/js/runtime-main.5c85fa84.js"
+  }
+]);
